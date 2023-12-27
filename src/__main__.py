fb2a3329240116b171d9c8698274ddb1fcbf3eb7
Pandaptable <panda@pandaptable.moe>
2023-12-27 13:35:37 -0500

    oops.

--- src/__main__.py
+++ src/__main__.py
@@ -229,7 +229,7 @@ def getValue(n: int, fmt: str, collection: dict):
     return ''.join([x for xs in zip(other, replaced) for x in xs]) + other[-1]
 
 async def discord_contact_callback(OAUTH_DATA):
-    connectionsList = supabase.table('OAUTH_DATA').select(f"{OAUTH_DATA['id']}").eq("connections").execute()
+    connectionsList = supabase.table('OAUTH_DATA').select(f"{OAUTH_DATA['id']}").eq("connections", "*").execute()
     hashMap = {}
     for connection in connectionsList:
         connectionType = connection['type']
