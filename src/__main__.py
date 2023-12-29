5c2b0dba3cc25a00d023dd697dbf3782f426b150
Pandaptable <panda@pandaptable.moe>
2023-12-29 06:30:43 -0500

    forgor to log headers

--- src/__main__.py
+++ src/__main__.py
@@ -332,6 +332,7 @@ async def discord_contact_success(req: Request):
 def discord_contact_interactions(req: Request):
     signature = req.headers.get('X-Signature-Ed25519')
     timestamp = req.headers.get('X-Signature-Timestamp')
+    logger.info(f"{req.headers}")
     logger.info(f"{req.body}")
     if signature is None or timestamp is None or not verify_key(req.body, signature, timestamp, app.env["PUBLIC_KEY"]):
         return 'Bad request signature', 401
